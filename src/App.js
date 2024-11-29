4f0b8185eacada257eedfae3a96756b5bf35a37d
David <hartleydavid13~gmail.com>
2024-11-29 14:12:09 +0000

    restructured RugbyPitch.js to new components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { ReactSVG } from 'react-svg';
-import ScrumStats from './components/ScrumStats';
-import KickStats from './components/KickStats';
-import LineoutStats from './components/LineoutStats';
-import RugbyPitch  from './components/RugbyPitch';
-import Rugby_Pitch from './components/Rugby_Pitch';
+import ScrumStats from './components/Actions/ScrumStats';
+import KickStats from './components/Actions/KickStats';
+import LineoutStats from './components/Actions/LineoutStats';
+import RugbyPitch from './components/InteractivePitch/RugbyPitch';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Rugby Performance Tracker</h1>
-      <div className="pitch-container">
-        <Rugby_Pitch width="100" height="100"/>
-      </div>
       <div className="stats-container">
         <ScrumStats />
         <KickStats />
---
+++ src/components/InteractivePitch/RugbyPitch.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PitchSelection from './PitchSelection';
+import ActionMenu from '../ActionMenu/ActionMenu';
+import ScrumStats from '../Actions/ScrumStats';
+import KickStats from '../Actions/KickStats';
+import LineoutStats from '../Actions/LineoutStats';
+
+function RugbyPitch() {
+  // State to store selected area and action type
+  const [selectedArea, setSelectedArea] = useState(null);
+  const [actionType, setActionType] = useState(null);
+
+  // handle selectedArea onClick event
+  const handleAreaSelect = (area) => {
+    setSelectedArea(area); // Fixed the issue here
+    setActionType(null);
+  };
+
+  const handleActionSelect = (action) => {
+    setActionType(action);
+  };
+
+  return (
+    <div>
+      <h1>Rugby Performance Tracker</h1>
+
+      {/* Rugby pitch with clickable sections */}
+      <PitchSelection onAreaSelect={handleAreaSelect} />
+
+      {/* Display action menu when area is selected */}
+      {selectedArea && !actionType && (
+        <ActionMenu onActionSelect={handleActionSelect} />
+      )}
+
+      {/* Display local stats based on selected action */}
+      {actionType === 'scrum' && <ScrumStats />}
+      {actionType === 'kick' && <KickStats />}
+      {actionType === 'lineout' && <LineoutStats />}
+    </div>
+  );
+}
+
+export default RugbyPitch;
